e5de3a3d70e9f4be9e4df1bbf5135e825b9b3e54
Sankalp Gupta <sankalpgupta444@gmail.com>
2025-02-05 11:17:45 +0530

    Added reddis server and secret key

--- client/client/settings.py
+++ client/client/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-54n%!m$m_09pcg!z%4_l8shgut)z)=yy8cp7rr)=etetn909r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
