ef359afd893f781af8cf4773304fcfdbc3cab7fb
Sankalp Gupta <sankalpgupta444@gmail.com>
2025-02-11 23:56:54 +0530

    env updates

--- client/client/settings.py
+++ client/client/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 from decouple import config
 import dj_database_url
 import os
-from dotenv import load_dotenv
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
