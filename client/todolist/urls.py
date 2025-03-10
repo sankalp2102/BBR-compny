a511675499fdc7ca14bcb3c5fa27cc57c3465090
Sankalp Gupta <sankalpgupta444@gmail.com>
2025-03-10 21:37:16 +0530

    superuser created

--- client/todolist/urls.py
+++ client/todolist/urls.py
@@ -10,6 +10,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib import admin
 
 
 schema_view = get_schema_view(
@@ -26,6 +27,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
